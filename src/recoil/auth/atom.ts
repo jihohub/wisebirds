afce552bbb8ce9bac2e996a3bc2a138766aa7bda
Jiho Park <diorpark@gmail.com>
2024-03-04 16:04:23 +0900

    Fix: 어드민이 아닌 매니저나 뷰어인 상태에서 /users로 직접 이동을 시도할 때 리다이렉션 되도록 수정

--- src/recoil/auth/atom.ts
+++ src/recoil/auth/atom.ts
@@ -1,8 +1,12 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 const authState = atom<string>({
   key: "authState",
   default: "admin",
+  effects_UNSTABLE: [persistAtom],
 });
 
 export default authState;
